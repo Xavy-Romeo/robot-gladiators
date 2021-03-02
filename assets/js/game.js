ebc103a476136a179e183b19a7a544162e02ef72
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-03-02 11:30:59 -0800

    shop options changed to integers

--- assets/js/game.js
+++ assets/js/game.js
@@ -220,27 +220,23 @@ var fight = function(enemy) {
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
+
+  shopOptionPrompt = parseInt(shopOptionPrompt);
 
   // use switch to carry out action
   switch (shopOptionPrompt) {
     
-    case "refill":
-    case "REFILL": // new case
-    case "Refill": // new case
+    case 1:
       playerInfo.refillHealth();
       break;
 
-    case "upgrade":
-    case "UPGRADE": // new case
-    case "Upgrade": // new case
+    case 2:
       playerInfo.upgradeAttack();
       break;
 
-    case "leave":
-    case "LEAVE": // new case
-    case "Leave": // new case
+    case 3:
       window.alert("Leaving the store.")  
       // do nothing, so shop() function will end
       break;
